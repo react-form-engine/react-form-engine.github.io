43bb387107b3b62fefb4a2ecabe5b8d2fbab9742
Richard Ng <richard.ng@MC16.local>
2019-11-14 01:08:49 +0800

    add build folder

---
+++ build/precache-manifest.32e5fff545d18c9748f1adfc337e93da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79c3eea4e8fca6f09709b71847cb5c1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "506c3f9e7b12abf9b220",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "20c1ca0b4609c642f9cb",
+    "url": "/static/js/2.2af16dc5.chunk.js"
+  },
+  {
+    "revision": "506c3f9e7b12abf9b220",
+    "url": "/static/js/main.09d2cfaf.chunk.js"
+  },
+  {
+    "revision": "bd4f286ef5e3e40628f9",
+    "url": "/static/js/runtime-main.472749e6.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
