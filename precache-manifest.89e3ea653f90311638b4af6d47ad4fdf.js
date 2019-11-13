1fd21b8e7ccef0d3bfc428f121bae4a90a4e145c
Richard Ng <richard.ng@MC16.local>
2019-11-14 01:51:48 +0800

    (auto)publish

---
+++ precache-manifest.89e3ea653f90311638b4af6d47ad4fdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbfbe396c9ac5a5c0b0843a2d699f9fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a45c535b9205a6f3a60",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d67613fcf013fdd32f3b",
+    "url": "/static/js/2.e42d3b11.chunk.js"
+  },
+  {
+    "revision": "2a45c535b9205a6f3a60",
+    "url": "/static/js/main.6b8bb6fd.chunk.js"
+  },
+  {
+    "revision": "67d09feca5ad9308ba6c",
+    "url": "/static/js/runtime-main.9445519e.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
